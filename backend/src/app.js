44ae9a8329d3493b6beefa32225cc4fa876f72d3
Wimaaaa <bernardo.wiemer333@gmail.com>
2026-05-01 20:15:19 -0300

    sistema de amizades, zoom e camadas no mapa

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,11 +5,13 @@ const app = express();
 const authRoutes = require('./routes/authRoutes');
 const eventRoutes = require('./routes/eventRoutes');
 const complaintRoutes = require('./routes/complaintRoutes');
+const friendshipRoutes = require('./routes/friendshipRoutes');
 
 app.use(express.json());
 app.use(cors());
 app.use('/api/auth', authRoutes);
 app.use('/api/events', eventRoutes);
 app.use('/api/complaints', complaintRoutes);
+app.use('/api/friendships', friendshipRoutes);
 
 module.exports = app;
